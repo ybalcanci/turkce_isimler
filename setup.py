180810efa280283d248856cd49d79fabd2044ce9
Emre Sülün <emresulun93@gmail.com>
2019-11-29 17:40:23 +0300

    Bişeyler

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from distutils.core import setup
 setup(
     name="turkce_isimler",
     packages=["turkce_isimler"],
-    version="0.1",
+    version="1.2",
     license="MIT",
     description="Bu paket ile rastgele Türkçe kadın ve erkek isimleri üretebilirsiniz.",
     author="Yasin Balcancı, Emre Sülün",
     author_email="ybalcanci@gmail.com",
     url="https://github.com/ybalcanci/turkce_isimler",
-    download_url='https://github.com/ybalcanci/turkce_isimler/archive/1.1.tar.gz',
+    download_url='https://github.com/ybalcanci/turkce_isimler/archive/1.2.tar.gz',
     keywords=["TURKISH", "MOCK-DATA", "İSİM"],
     install_requires=[],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "Topic :: Natural Language :: Turkish",
         'License :: MIT License',
         'Programming Language :: Python :: 3'
     ],
