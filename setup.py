3fbb01862b00fafc1ba23bc4223873568a7a755b
Yasin Balcancı <36000428+ybalcanci@users.noreply.github.com>
2019-11-29 17:23:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-    name="turkce-sozde-insan-ismi-uretici",
-    packages=["turkce-sozde-insan-ismi-uretici"],
+    name="turkce_isimler",
+    packages=["turkce_isimler"],
     version="0.1",
     license="MIT",
     description="Bu paket ile rastgele Türkçe kadın ve erkek isimleri üretebilirsiniz.",
     author="Yasin Balcancı, Emre Sülün",
     author_email="ybalcanci@gmail.com",
-    url="https://github.com/ybalcanci/turkce-sozde-insan-ismi-uretici",
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    url="https://github.com/ybalcanci/turkce_isimler",
+    download_url='https://github.com/ybalcanci/turkce_isimler/archive/1.0.tar.gz',
     keywords=["TURKISH", "MOCK-DATA", "İSİM"],
     install_requires=[],
     classifiers=[
